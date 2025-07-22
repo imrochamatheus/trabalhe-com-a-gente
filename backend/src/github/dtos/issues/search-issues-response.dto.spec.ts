b4a3d5a684489fe744e285ab3b09dcec267650f0
Matheus Rocha <im.rochamatheus@gmail.com>
2025-07-22 13:03:28 -0300

    :trash: chore: remover arquivos de teste inutilizados

--- backend/src/github/dtos/issues/search-issues-response.dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SearchIssuesResponseDto } from "./search-issues-response.dto";
-
-describe("SearchIssuesResponseDto", () => {
-  it("should be defined", () => {
-    expect(new SearchIssuesResponseDto()).toBeDefined();
-  });
-});
--- backend/src/github/dtos/repositories/search-repositories-response.dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SearchRepositoriesResponseDto } from "./search-repositories-response.dto";
-
-describe("SearchRepositoriesResponseDto", () => {
-  it("should be defined", () => {
-    expect(new SearchRepositoriesResponseDto()).toBeDefined();
-  });
-});
