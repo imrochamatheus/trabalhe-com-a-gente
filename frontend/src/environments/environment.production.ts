3056a9a70e9d316edfc1295f6c77cc76bb24bd7b
Matheus Rocha <im.rochamatheus@gmail.com>
2025-07-25 17:05:07 -0300

    :recycle: refactor: alterar porta de acesso aos endpoints nas variáveis de ambiente

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://localhost:8200/github",
+  apiUrl: "http://localhost:3000/github",
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:8200/github",
+  apiUrl: "http://localhost:3000/github",
 };
